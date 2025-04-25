03f87509274329b2259c12e40baf25c179ed0d72
Nicolecw <nicole.wilson.2@fwcsstudents.org>
2025-04-25 13:17:45 -0400

    Add files via upload

--- weather_info.py
+++ weather_info.py
@@ -1,5 +1,6 @@
 import csv
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 path = Path('weather_data.csv')
 lines = path.read_text().splitlines()
@@ -19,10 +20,38 @@ for row in reader:
         if key == year:
             temps[year].append(temp)
 
-print(temps)
+dates = ["July 18",
+         "July 19",
+         "July 20",
+         "July 21",
+         "July 22",]
 
-#for year in temps.keys():
-#    for row in reader:
-#        if int(row[0]) == year:
-#            temp = int(row[2])
-#            temps[year].append(temp)
+colors = ["red",
+          "green",
+          "blue",
+          "cyan",
+          "magenta",
+          "orange",
+          "yellow",
+          "black",
+          "purple",
+          "maroon"]
+
+for year, color in zip(range(2015,2025), colors):
+        plt.plot(dates, temps[year], linestyle='--', marker='o', color=color, label=year)
+plt.title('10 Years of Temperatures')
+plt.xlabel('Dates')
+plt.ylabel('Temperature (Degrees Fahrenheit)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+
+#plt.plot(year, temp, linestyle='-', marker='o', color='red', label='Data')
+#plt.title('Temperature')
+#plt.xlabel('Year')
+#plt.ylabel('Temp')
+#plt.legend()
+#plt.grid(True)
+#plt.show()
